ba09cfed360a0a78d5e692a3eea2b1599ff35454
Johnny-yellowboy <389036572@qq.com>
2020-11-29 21:53:40 +0800

    报表

---
+++ js/report.js
@@ -0,0 +1,34 @@
+var chart = Highcharts.chart('report-chart', {
+	chart: {
+		type: 'pie',
+		options3d: {
+			enabled: true,
+			alpha: 45
+		}
+  },
+  credits: {
+		enabled:false
+	},
+	title: {
+		text: ''
+	},
+	subtitle: {
+		text: ''
+	},
+	plotOptions: {
+		pie: {
+			innerSize: 100,
+			depth: 45
+		}
+	},
+	series: [{
+		name: '货物金额',
+		data: [
+			['在用', 8],
+			['闲置', 3],
+			['调拨中', 1],
+			['维修中', 6],
+			['清理报废', 8]
+		]
+	}]
+});
