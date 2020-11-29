9a60baeea5866e8780eb37dfc11a431fb0a02227
hohoto <540172574@qq.com>
2020-11-30 00:17:11 +0800

    addpandianHTML

---
+++ js/pandian/category.js
@@ -0,0 +1,44 @@
+let pandianlist
+
+
+let getPandianList = () => {
+        console.log("开始获取盘点单")
+        $.ajax({
+            url: "/pandian/getpandian",
+            type: "get",
+            dataType: "json",
+            success: function (text) {
+                pandianlist = text
+                
+                pandianlist.length = pandianlist.length > 3 ? 3 : pandianlist.length
+                if (pandianlist.length >= 1) {
+                    $('#li1').text('盘点单:' + pandianlist[0].pandianCode)
+                    $('#li1').on('click', () => {
+                        window.location = "./pandiandetail.html"
+                    })
+                }
+                if (pandianlist.length >= 2) {
+                    $('#li2').text('盘点单:' + pandianlist[1].pandianCode)
+                    $('#li2').on('click', () => {
+                        window.location = "./pandiandetail.html" 
+                    })
+                }
+                if (pandianlist.length >= 3) {
+                    $('#li3').text('盘点单:' + pandianlist[2].pandianCode)
+                    $('#li3').on('click', () => {
+                        window.location = "./pandiandetail.html" 
+                    })
+                }
+                if (pandianlist.length < 3) {
+                    if (pandianlist.length <= 2) $('#li3').css('display','none') 
+                    if (pandianlist.length <= 1) $('#li2').css('display','none') 
+                    if (pandianlist.length <= 0) $('#li1').css('display','none')
+                }
+            },
+            error: function () {
+                console.log('获取盘点单失败')
+            }
+        })
+}
+
+setTimeout(() => {getPandianList()},0)
---
+++ js/pandian/createpandiandetail.js
@@ -0,0 +1,32 @@
+let getData = () => {
+    let data = {
+        pandianDetailCode: $('#pandianDetailCode').val(),
+        resourceId: $('#resourceId').val(),
+        locationCode: $('#locationCode').val()
+    }
+    return JSON.stringify(data)
+}
+
+
+
+
+let bindevent = () => {
+    $('#createpandian').on('click', () => { 
+        let data = getData()
+        $.ajax({
+            url: "/pandian/addpandiandetail",
+            contentType: "application/json;charset=UTF-8",
+            data: data,
+            type: "post",
+            dataType: "json",
+            success: function (text) {
+                if(text = "true") window.confirm("新增成功")
+            },
+            error: function () {
+                console.log('创建盘点单失败')
+            }
+        })
+    })
+}
+
+setTimeout(() => {bindevent()},0)
